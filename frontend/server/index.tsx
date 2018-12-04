c41e435d91e6cb56df434e1869a5dbdbcb8bb2bd
Daniel Ternyak <dternyak@gmail.com>
2018-12-04 17:01:37 -0600

    setup in-app ssl redirects for Heroku

--- frontend/server/index.tsx
+++ frontend/server/index.tsx
@@ -7,6 +7,7 @@ import * as bodyParser from 'body-parser';
 import expressWinston from 'express-winston';
 import i18nMiddleware from 'i18next-express-middleware';
 import * as Sentry from '@sentry/node';
+import * as enforce from 'express-sslify';
 
 import '../config/env';
 // @ts-ignore
@@ -25,6 +26,9 @@ Sentry.init({
 });
 
 const app = express();
+
+// ssl
+app.use(enforce.HTTPS());
 
 // sentry
 app.use(Sentry.Handlers.requestHandler());
